551e221fabdcabd2853a08c29b959934b5be3f5d
Nida Amir <142287805+Nidaamir083@users.noreply.github.com>
2025-08-05 00:03:00 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='code_gen_project',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'openpyxl',
+        'pandas',
+    ],
+    author='Nida Amir',
+    author_email='nida.amir0083@gmail.com',
+    description='A project that performs data analysis and visualization from Excel files',
+    long_description=open('README.md').read() if os.path.exists('README.md') else '',
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/your-repo-name',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
